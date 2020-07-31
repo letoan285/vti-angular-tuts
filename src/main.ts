859f6f37047efd109e13af7545078226f4901b86
toan.le <toanlv@smartosc.com>
2020-07-31 09:44:49 +0700

    update header

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import './assets/scss/styles.scss';
 
 if (environment.production) {
   enableProdMode();
